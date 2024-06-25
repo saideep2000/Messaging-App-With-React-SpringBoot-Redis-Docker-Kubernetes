f31a1af65b6f99dbffa5d1097ec7a57c2c29f867
saideepsamineni <saminenisaideepchowdary@gmail.com>
2024-06-25 17:22:18 -0400

    Done naming

--- backend/src/main/java/com/example/demo/controller/MessageController.java
+++ backend/src/main/java/com/example/demo/controller/MessageController.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/backend/api/messages")
+@RequestMapping("/sproutbackend/api")
 public class MessageController {
 
   @Autowired
